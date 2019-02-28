42fbf4b427d7c8c5498e73d2504c0876032c2d63
github username <github email address>
2019-02-28 11:37:00 +0000

    small change for test git

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            New Kodflix app
+            Welcome to Kodflix_v2
           </a>
         </header>
       </div>

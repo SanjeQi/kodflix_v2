9a6db882a8a9a318e1468aefd096be0b6b21f797
github username <github email address>
2019-03-01 12:08:38 +0000

    Add 6 TV shows titles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
-import seinfeld from "./seinfeld.jpg";
+// import seinfeld from "./covers/seinfeld.jpg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={seinfeld} alt="seinfeld_img" />
+        <div className="image-cover-row">
+          <div className="image-cover-row-item">
+            <h1>Black Mirror</h1>
+          </div>
+        </div>
+        <div className="image-cover-row">
+          <div className="image-cover-row-item">
+            <h1>Breaking Bad</h1>
+          </div>
+        </div>
+        <div className="image-cover-row">
+          <div className="image-cover-row-item">
+            <h1>Seinfeld</h1>
+          </div>
+        </div>
+        <div className="image-cover-row">
+          <div className="image-cover-row-item">
+            <h1>Games of Thrones</h1>
+          </div>
+        </div>
+        <div className="image-cover-row">
+          <div className="image-cover-row-item">
+            <h1>Generation Kill</h1>
+          </div>
+        </div>
+        <div className="image-cover-row">
+          <div className="image-cover-row-item">
+            <h1>The Wire</h1>
+          </div>
+        </div>
       </div>
     );
   }

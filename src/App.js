7e04a534601014dd7b868f3ce8c26acf9ec4bebc
github username <github email address>
2019-03-12 17:22:45 +0000

    Add the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -15,24 +15,42 @@ class App extends Component {
         <div className="image-cover-row">
           <div className="image-cover-row-item">
             <img src={black} alt="black" />
+            <div className="image-cover-row-item-overlay">
+              <h1>Black Mirror</h1>
+            </div>
           </div>
           <div className="image-cover-row-item">
             <img src={got} alt="got" />
+            <div className="image-cover-row-item-overlay">
+              <h1>Games of Thrones</h1>
+            </div>
           </div>
           <div className="image-cover-row-item">
             <img src={bad} alt="bad" />
+            <div className="image-cover-row-item-overlay">
+              <h1>Breaking Bad</h1>
+            </div>
           </div>
         </div>
 
         <div className="image-cover-row">
           <div className="image-cover-row-item">
             <img src={kill} alt="kill" />
+            <div className="image-cover-row-item-overlay">
+              <h1>Generation Kill</h1>
+            </div>
           </div>
           <div className="image-cover-row-item">
             <img src={sein} alt="sein" />
+            <div className="image-cover-row-item-overlay">
+              <h1>Seinfeld</h1>
+            </div>
           </div>
           <div className="image-cover-row-item">
             <img src={wire} alt="wire" />
+            <div className="image-cover-row-item-overlay">
+              <h1>The Wire</h1>
+            </div>
           </div>
         </div>
       </div>

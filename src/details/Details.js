f6183c9ef1fd0d28899b22885b4c3a3b172b2234
github username <cristi.nord@gmail.com>
2019-03-13 17:12:03 +0000

    Add basic Routing/Navigation / Create details component #8

---
+++ src/details/Details.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Details extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello, this is the detail page for each TV Show. Under construction .
+          Please return at a alter date
+        </h1>
+        <Link to="/">Go Back to HomePage</Link>
+      </div>
+    );
+  }
+}
+
+export default Details;
--- src/gallery/cover/Cover.js
+++ src/gallery/cover/Cover.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Cover.css";
+import { Link } from "react-router-dom";
 
 export default class Cover extends React.Component {
   render() {
     return (
-      <div className="cover">
+      <Link to="/details" className="cover">
         <img src={this.props.image} alt="" />
         <div className="cover-overlay">
           <h1>{this.props.title}</h1>
         </div>
-      </div>
+      </Link>
     );
   }
 }

6821407ca33812df5496d29e95d4511f7133b86f
github username <cristi.nord@gmail.com>
2019-03-13 16:03:34 +0000

    Create styling for Gallery component

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -1,25 +1,26 @@
 import React from "react";
 
-import Cover from "../cover/Cover";
+import Cover from "./cover/Cover";
+import "./Gallery.css";
 
-import black from "../cover/images/black.jpg";
-import got from "../cover/images/got.jpg";
-import bad from "../cover/images/bad.jpg";
-import kill from "../cover/images/kill.jpg";
-import sein from "../cover/images/sein.jpg";
-import wire from "../cover/images/wire.jpg";
+import black from "./cover/images/black.jpg";
+import got from "./cover/images/got.jpg";
+import bad from "./cover/images/bad.jpg";
+import kill from "./cover/images/kill.jpg";
+import sein from "./cover/images/sein.jpg";
+import wire from "./cover/images/wire.jpg";
 
 class Gallery extends React.Component {
   render() {
     return (
-      <div>
-        <div className="image-cover-row">
+      <div className="gallery">
+        <div className="gallery-row">
           <Cover image={black} title={"Black Mirror"} />
           <Cover image={got} title={"Games of Thrones"} />
           <Cover image={bad} title={"Breaking Bad"} />
         </div>
 
-        <div className="image-cover-row">
+        <div className="gallery-row">
           <Cover image={kill} title={"Generation Kill"} />
           <Cover image={sein} title={"Seinfeld"} />
           <Cover image={wire} title={"The Wire"} />

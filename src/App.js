601fa2bd8a910bd0ee50cc2596d0395469027155
github username <github email address>
2019-03-13 12:44:02 +0000

    Componentize the TV show element #7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import bad from "./covers/bad.jpg";
-import black from "./covers/black.jpg";
-import got from "./covers/got.jpg";
-import kill from "./covers/kill.jpg";
-import sein from "./covers/sein.jpg";
-import wire from "./covers/wire.jpg";
+import Cover from "./cover/Cover";
+import black from "./cover/images/black.jpg";
+import got from "./cover/images/got.jpg";
+import bad from "./cover/images/bad.jpg";
+import kill from "./cover/images/kill.jpg";
+import sein from "./cover/images/sein.jpg";
+import wire from "./cover/images/wire.jpg";
 
 import "./App.css";
 
@@ -13,45 +14,15 @@ class App extends Component {
     return (
       <div className="App">
         <div className="image-cover-row">
-          <div className="image-cover-row-item">
-            <img src={black} alt="black" />
-            <div className="image-cover-row-item-overlay">
-              <h1>Black Mirror</h1>
-            </div>
-          </div>
-          <div className="image-cover-row-item">
-            <img src={got} alt="got" />
-            <div className="image-cover-row-item-overlay">
-              <h1>Games of Thrones</h1>
-            </div>
-          </div>
-          <div className="image-cover-row-item">
-            <img src={bad} alt="bad" />
-            <div className="image-cover-row-item-overlay">
-              <h1>Breaking Bad</h1>
-            </div>
-          </div>
+          <Cover image={black} title={"Black Mirror"} />
+          <Cover image={got} title={"Games of Thrones"} />
+          <Cover image={bad} title={"Breaking Bad"} />
         </div>
 
         <div className="image-cover-row">
-          <div className="image-cover-row-item">
-            <img src={kill} alt="kill" />
-            <div className="image-cover-row-item-overlay">
-              <h1>Generation Kill</h1>
-            </div>
-          </div>
-          <div className="image-cover-row-item">
-            <img src={sein} alt="sein" />
-            <div className="image-cover-row-item-overlay">
-              <h1>Seinfeld</h1>
-            </div>
-          </div>
-          <div className="image-cover-row-item">
-            <img src={wire} alt="wire" />
-            <div className="image-cover-row-item-overlay">
-              <h1>The Wire</h1>
-            </div>
-          </div>
+          <Cover image={kill} title={"Generation Kill"} />
+          <Cover image={sein} title={"Seinfeld"} />
+          <Cover image={wire} title={"The Wire"} />
         </div>
       </div>
     );

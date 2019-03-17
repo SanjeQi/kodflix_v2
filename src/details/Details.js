c0c27d832b2d73ab81008ced7420ac35a10aa083
github username <cristi.nord@gmail.com>
2019-03-17 16:52:45 +0000

    Introduce state in Details component #13

--- src/details/Details.js
+++ src/details/Details.js
@@ -2,13 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Details extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      welcomeMessage:
+        "Hello, this is the detail page for each TV Show. Under construction . Please return at a later date"
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        welcomeMessage: "The best is yet to come. Under construction"
+      });
+    }, 3000);
+  }
+
   render() {
     return (
       <div>
-        <h1>
-          Hello, this is the detail page for each TV Show. Under construction .
-          Please return at a later date
-        </h1>
+        <h1>{this.state.welcomeMessage}</h1>
         <Link to="/">Go Back to HomePage</Link>
       </div>
     );

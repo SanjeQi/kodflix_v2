f1cbee3c92f945788c3803d495842ff4aad82e7a
github username <cristi.nord@gmail.com>
2019-03-24 18:49:12 +0000

    Return a list of shows from the backend #19

---
+++ src/backend/shows.js
@@ -0,0 +1,50 @@
+module.exports = function getShows() {
+  return [
+    {
+      id: "black",
+      title: "Black Mirror",
+      synopsis: `
+            An anthology series exploring a twisted, high-tech world where 
+            humanity's greatest innovations and darkest instincts collide.`
+    },
+    {
+      id: "got",
+      title: "Games of Thrones",
+      synopsis: `
+            Nine noble families fight for control over the mythical lands of 
+            Westeros, while an ancient enemy returns after being dormant 
+            for thousands of years.`
+    },
+    {
+      id: "bad",
+      title: "Breaking Bad",
+      synopsis: `
+            A high school chemistry teacher diagnosed with inoperable 
+            lung cancer turns to manufacturing and selling methamphetamine 
+            in order to secure his family's future.`
+    },
+    {
+      id: "kill",
+      title: "Generation Kill",
+      synopsis: `
+              A Rolling Stone reporter, embedded with The 1st Recon Marines 
+              chronicles his experiences during the first wave of the American-led 
+              assault on Baghdad in 2003`
+    },
+    {
+      id: "sein",
+      title: "Seinfeld",
+      synopsis: `
+              The continuing misadventures of neurotic New York City 
+              stand-up comedian Jerry Seinfeld and his equally neurotic 
+              New York City friends.`
+    },
+    {
+      id: "wire",
+      title: "The Wire",
+      synopsis: `
+              Baltimore drug scene, seen through the eyes of drug 
+              dealers and law enforcement.`
+    }
+  ];
+};

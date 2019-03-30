dafff47ab8795b69dd068cd5ebb4080dde7fcd5f
github username <cristi.nord@gmail.com>
2019-03-30 17:11:36 +0000

    Enable backend server in prod#23

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const port = process.env.PORT || 3001;
 const getShows = require("./shows");
 
 app.get("/rest/shows", (req, res) => res.send(getShows()));
 
-app.listen(3001, () => console.log("Example app listening on port 3001!"));
+//Serve any static files
+app.use(express.static(path.join(__dirname, "../../build")));
+
+//Handel React Routing, return all requests to React App
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../../build", "index.html"));
+});
+
+app.listen(port, () => console.log(`Listening to port ${port}`));

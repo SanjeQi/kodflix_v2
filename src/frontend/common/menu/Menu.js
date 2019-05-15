c1bd78522cc98f1984a1cdea75e04c69178c1065
github username <cristi.nord@gmail.com>
2019-05-15 21:18:40 +0100

    change menu and styling

--- src/frontend/common/menu/Menu.js
+++ src/frontend/common/menu/Menu.js
@@ -1,44 +1,43 @@
-import React from "react";
-import MenuLink from "./menu-link/MenuLink";
-import menuIcon from "./menu.svg";
+import React, { Component } from "react";
 import "./Menu.css";
 
-export default class Menu extends React.Component {
+export default class Menu extends Component {
   constructor() {
     super();
-    this.state = { isMenuVisible: false };
+    this.state = {
+      showMenu: false,
+      dropDownMenu: ""
+    };
+    this.menuDropdown = this.menuDropdown.bind(this);
   }
 
-  toggleMenu() {
-    this.setState({ isMenuVisible: !this.state.isMenuVisible });
+  menuDropdown() {
+    this.setState({ showMenu: !this.state.showMenu });
   }
 
   render() {
     return (
-      <div className={"menu " + (this.state.isMenuVisible ? "is-visible" : "")}>
-        <button className="menu-toggle" onClick={() => this.toggleMenu()}>
-          <img src={menuIcon} alt="Open menu" />
-        </button>
-        <div className="menu-panel">
-          <div className="menu-panel-box">
-            <MenuLink
-              text="Home"
-              link="/"
-              icon="home"
-              onClick={() => this.toggleMenu()}
-            />
-            <MenuLink
-              text="Manage TV Shows"
-              link="/manage/tv-shows"
-              icon="tv"
-              onClick={() => this.toggleMenu()}
-            />
-          </div>
-          <div
-            className="menu-panel-overlay"
-            onClick={() => this.toggleMenu()}
-          />
+      <div className="menu-container">
+        <div
+          className={["menu", this.state.showMenu ? "toggle-menu" : ""].join(
+            " "
+          )}
+        >
+          <svg
+            onClick={this.menuDropdown}
+            className="btn"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="rgb(200, 203, 209)"
+          >
+            <path d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z" />
+          </svg>
+          <div className="options">Hello</div>
         </div>
+        <div
+          className={this.state.showMenu ? "overlay" : ""}
+          onClick={this.menuDropdown}
+        />
       </div>
     );
   }

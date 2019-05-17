47a81d56c00e50b4b6343052da0c7f5f46b52dab
github username <cristi.nord@gmail.com>
2019-05-17 17:03:13 +0100

    menu menu option

--- src/frontend/manage-tv-shows/ManageTvShows.js
+++ src/frontend/manage-tv-shows/ManageTvShows.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import "./ManageTvShows.css";
+import { Link } from "react-router-dom";
 
-class ManageTvShows extends React.Component {
+class ManageTvShows extends Component {
   render() {
-    return "Hello World";
+    return (
+      <div className="manage-shows-container">
+        <h1>Manage TV Shows Component</h1>
+        <Link to="/">
+          <h2 className="home">Home</h2>
+        </Link>
+      </div>
+    );
   }
 }
 

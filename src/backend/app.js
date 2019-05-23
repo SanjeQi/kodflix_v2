de358a3b9b0bff970ac6520513b140850c41917f
github username <cristi.nord@gmail.com>
2019-05-23 20:47:06 +0100

    backend_change

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,34 +1,36 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-const db = require("./db");
+const path = require("path");
 const port = process.env.PORT || 3001;
+const connect = require("./db");
 
-db.connect().then(dbo => {
+connect().then(dbo => {
   app.get("/rest/shows", (req, res) => {
     dbo
       .collection("shows")
       .find({})
-      .toArray((err, results) => {
+      .toArray(function(err, result) {
         if (err) throw err;
-        res.send(results);
+        res.send(result);
       });
   });
 
   app.get("/rest/shows/:id", (req, res) => {
-    dbo.collection("shows").findOne({ id: req.params.id }, (err, doc) => {
+    const iD = req.params.id;
+    dbo.collection("shows").findOne({ id: iD }, {}, function(err, result) {
       if (err) throw err;
-      res.send(doc);
+      if (!result) {
+        res.status(404).send({});
+      } else {
+        res.send(result);
+      }
     });
   });
 
-  // Serve any static files
   app.use(express.static(path.join(__dirname, "../../build")));
-
-  // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../../build", "index.html"));
   });
-
-  app.listen(port, () => console.log(`Listening on port ${port}`));
 });
+
+app.listen(port, () => console.log(`Server is listening to Port:${port}`));
